9ba8e34ed8cb9faa1c8fd746360f0e5286c4c82c
VishmikaMaduka <wishmika@wso2telco.com>
2016-01-27 18:12:19 +0530

    jaggery apps are uptodate with indian node

--- modules/jaggery-apps/dashboard/js/waiting.js
+++ modules/jaggery-apps/dashboard/js/waiting.js
@@ -1,3 +1,7 @@
+var msisdn=values["msisdn"];
+var tokenVal=values["token"];
+console.log(msisdn+" : "+tokenVal);
+
 var timeout = 60000;
 var pollingInterval = 2000;
 var timeRemaining = timeout;
@@ -5,24 +9,30 @@ var hasResponse = false;
 var isTimeout = false;
 var status='pending';
 var sessionId;
+console.log(" timeout : " +timeout+" pollingInterval : " +pollingInterval+" timeRemaining : " +timeRemaining+" hasResponse : " +hasResponse+" isTimeout : " +isTimeout+" status : " +status);
 
 var pollingVar = setInterval(pollForStatus, pollingInterval);
+console.log("waiting");
 
 /* 
  * Check for USSD response status if timeout not is reached 
  * or user approval status(USSD) is not specified (YES/NO).
  */
-function pollForStatus() {
+ function pollForStatus() {
+ 	console.log(" timeout : " +timeout+" pollingInterval : " +pollingInterval+" timeRemaining : " +timeRemaining+" hasResponse : " +hasResponse+" isTimeout : " +isTimeout+" status : " +status);
+
 
 	// If timeout has not reached.
 	if(timeRemaining > 0) {
 		// If user has not specified a response(YES/NO).
+		
 		if(!hasResponse) {
 			checkUSSDResponseStatus();
 			timeRemaining = timeRemaining - pollingInterval;
 			
 		} else {
 			handleTermination();
+			
 		}
 		
 	} else {
@@ -34,153 +44,167 @@ function pollForStatus() {
 /*
  * Handle polling termination and form submit.
  */
-function handleTermination() {
-	//window.clearInterval(pollingVar);
-	//window.open("./landing.html",'_self',"User Registration");
-	
-	window.clearInterval(pollingVar);
-	var STATUS_PENDING = "pending";
-	if(!status==STATUS_PENDING){
-		$('#waiting_screen_success').show();
-	}
-	$('#waiting_screen').hide();
-	//setTimeout(redirectBack(), 5000);
-	setTimeout(function(){
-        redirectBack();
-    }, 5000);
-}
+ function handleTermination() {
+
+ 	window.clearInterval(pollingVar);
+ 	var STATUS_PENDING = "pending";
+ 	if(!status==STATUS_PENDING){
+ 		$('.page__header').show();
+ 		$('#sms_fallback').show();
+ 	}
+ 	$('.page__header').hide();
+ 	$('#sms_fallback').hide();
+
+ 	setTimeout(function(){
+ 		redirectBack();
+ 	}, 5000);
+
+
+
+ }
 
 /*
  * Redirect after end of registration
  */
+
+ 
  function redirectBack() {
     // Get the value of the 'loginRequestURL' cookie
     var loginURL = decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + "loginRequestURL" + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-    if(!loginURL) {
-    	window.location.href = "./landing.html";
-    } else {
-    	if(tokenid){
+
+
+    if(tokenVal){
     	selfAuthorize();
-                 }else{
-    	window.location.href = loginURL;
-    	         }
-    }
+    }else{
+
+    	if(isTimeout){
+    		window.location.href = "./landing.jag";
+    	}else{
+    		window.location.href = "./account-setup-success.jag";
+    	}
+
+    } 
+
+
 }
 
 
- /*
+/*
  * Invoke the endpoint for self authenticate.
  */
-function selfAuthorize(){
-      var callbackURL;
-      var acr;
-      var authendpoint;
-      var token;
-      var scope;
-      var id=qs("tokenid");
-      var username=qs("username");
-	  var url = "../UserRegistration-1.0-SNAPSHOT/webresources/endpoint/user/authenticate/get?tokenid="+ id;
-	
-	$.ajax({
-		type: "GET",
-		url:url,
-		async: false,
-	    dataType: 'json',
-		success:function(result){
-				if(result != null) {
-				scope = result.scope; 
-				callbackURL = result.redirectUri; 
-                clientkey = result.clientId; 
-                acr = result.acrValues; 
-                authendpoint = "../oauth2/authorize"; 
-                token = result.tokenID; 
-        }
-	}});
+ function selfAuthorize(){
+ 	var callbackURL;
+ 	var acr;
+ 	var authendpoint;
+ 	var token;
+ 	var scope;
+ 	var id=tokenVal;
+ 	var state;
+ 	var nonce;
+ 	var username=msisdn;
+ 	var url = "../UserRegistration-1.0-SNAPSHOT/webresources/endpoint/user/authenticate/get?tokenid="+ id;
 
-   var url = authendpoint + "?scope="+encodeURIComponent(scope)+"&response_type=code&redirect_uri="
-          + encodeURIComponent(callbackURL) + "&client_id=" + clientkey + "&acr_values=" 
-          + acr+"&tokenid="+token+"&msisdn="+username;
-      
-       window.location = url;
-}
+ 	$.ajax({
+ 		type: "GET",
+ 		url:url,
+ 		async: false,
+ 		dataType: 'json',
+ 		success:function(result){
+ 			if(result != null) {
+ 				scope = result.scope; 
+ 				callbackURL = result.redirectUri; 
+ 				state= result.state;
+ 				nonce=result.nonce;
+ 				clientkey = result.clientId; 
+ 				acr = result.acrValues; 
+ 				authendpoint = "../oauth2/authorize"; 
+ 				token = result.tokenID; 
+ 			}
+ 		}});
+
+ 	var url = authendpoint + "?scope="+encodeURIComponent(scope)+"&response_type=code&redirect_uri="
+ 	+ encodeURIComponent(callbackURL) + "&client_id=" + clientkey + "&acr_values=" 
+ 	+ acr+"&tokenid="+token+"&msisdn="+username+"&state="+state+"&nonce="+nonce + "&operator="+values["operator"];
+ 	console.log("url   " + url);
+ 	window.location = url;
+ }
+
+ function deleteUser(sessionId){
+ 	var deleteUserUrl = "/dashboard/delete_user.jag?username=" + sessionId;
+ 	$.ajax({
+ 		type: "GET",
+ 		url: deleteUserUrl,
+ 		async:false,
+ 	})
+ 	.done(function (data) {
+ 	})
+ 	.fail(function () {
+ 		console.log('error');
+ 	})
+ 	.always(function () {
+ 		console.log('completed');
+ 	});
+ }
 
 /*
  * Invoke the endpoint to retrieve USSD status.
  */
+ function checkUSSDResponseStatus() {
+ 	
+ 	sessionId=msisdn;
+ 	var url = "../UserRegistration-1.0-SNAPSHOT/webresources/endpoint/ussd/status?username=" + sessionId;
 
-function qs(key) {
-	
-    var vars = [], hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for(var i = 0; i < hashes.length; i++)
-    {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
-    }
-    return vars[key];
-}
+ 	var STATUS_APPROVED = "Approved";
 
-function deleteUser(sessionId){
-	var deleteUserUrl = "/dashboard/delete_user.jag?username=" + sessionId;
-		$.ajax({
-		type: "GET",
-		url: deleteUserUrl,
-		async:false,
-	})
-	.done(function (data) {
-	})
-	.fail(function () {
-		console.log('error');
-	})
-	.always(function () {
-		console.log('completed');
-});
-}
+ 	$.ajax({
+ 		type: "GET",
+ 		url:url,
+ 		async: false,
+ 		success:function(result){
+ 			if(result != null) {
+ 				var responseStatus = result.status; 
 
-function checkUSSDResponseStatus() {
-	
-	//var sessionId = document.getElementById('username').value;
-	sessionId=qs('username');
-	
-	///var url = "../MediationTest/tnspoints/endpoint/ussd/status?sessionID=" + sessionId;
-	var url = "../UserRegistration-1.0-SNAPSHOT/webresources/endpoint/ussd/status?username=" + sessionId;
-	var STATUS_APPROVED = "Approved";
-	
-	$.ajax({
-		type: "GET",
-		url:url,
-		async: false,
-		success:function(result){
-			if(result != null) {
-				var responseStatus = result.status; 
-					
-				if(responseStatus != null && responseStatus == STATUS_APPROVED) {
-					status = result.status;
-					hasResponse = true;
-				}
-			}
-	}});
-}
+ 				if(responseStatus != null && responseStatus == STATUS_APPROVED) {
+ 					status = result.status;
+ 					console.log("status : "+status);
+ 					hasResponse = true;
+ 				}
+ 			}
+ 		}});
+
+ }
 
 
-function resendUSSD(){
-	var msisdn=qs('username');
-	/*
-	$.ajax({
-    url: 'backend_service.jag',
-    type: 'GET',
-    error: function(){
-        alert('NOT EXISTS');
-    },
-    success: function(){
-        alert('EXISTS');
-    }
-	});
-	*/
-	var strBack = "backend_service.jag?msisdn=" + msisdn;
-	$.ajax({
-	    type: "GET",
-	    url: strBack
-	})
-}
+ function resendUSSD(){
+
+ 	var strBack = "backend_service.jag?msisdn=" + msisdn;
+ 	$.ajax({
+ 		type: "GET",
+ 		url: strBack
+ 	})
+ }
+
+/*
+ * when sms registration starts clear the polling values.
+ * if the status is still pending, start the sms registration.
+ */
+ function handleTerminationSms() {
+ 	window.clearInterval(pollingVar);
+ 	var STATUS_PENDING = "pending";
+ 	console.log(status +" = " +STATUS_PENDING);
+ 	if(status==STATUS_PENDING){
+ 		console.log('changed the flow');
+ 		console.log(" timeout : " +timeout+" pollingInterval : " +pollingInterval+" timeRemaining : " +timeRemaining+" hasResponse : " +hasResponse+" isTimeout : " +isTimeout+" status : " +status);
+ 		smsClick=true;
+ 		token=values["token"];
+ 		acr=values["acr"];
+ 		operator=values["operator"];
+ 		registration();
+
+ 	}else{
+ 		/*when sms link clicked if already registered using ussd*/
+ 		console.log('registered already');
+ 		pollForStatus();
+ 	}
+
+ }
